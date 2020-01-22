058d1957f58d542f2afa3cbd9073984ee7a29371
Satyo Wicaksana <satyowicaksana@gmail.com>
2020-01-23 01:54:03 +0700

    add service section header

---
+++ src/components/Services.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Services.scss'
+
+export default () => {
+  return (
+    <>
+      <div className="services">
+        <div className="container">
+          <h1>How Can I Help You?</h1>
+          <h3>
+            Our work then targeted, best practices outcomes social innovation synergy.
+            Venture philanthropy, revolutionary inclusive policymaker relief. User-centered
+            program areas scale.
+          </h3>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import NotificationPanel from '../components/NotificationPanel'
 import Hero from '../components/Hero'
+import Services from '../components/Services'
 
 export default () => {
   return (
     <>
       <NotificationPanel />
       <Hero />
-      <p>ini home</p>
+      <Services />
     </>
   )
 }
