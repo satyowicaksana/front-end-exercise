39a54489d2d8633cb6e4f06927cc4e725227eca9
Satyo Wicaksana <satyowicaksana@gmail.com>
2020-01-22 20:14:00 +0700

    slide notification panel

--- src/components/NotificationPanel.jsx
+++ src/components/NotificationPanel.jsx
@@ -1,24 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react'
 import './NotificationPanel.scss'
+import SlideToggle from 'react-slide-toggle'
 
 export default () => {
-  const [hide, setHide] = useState(false)
 
   return (
     <>
-      <div id="notification-panel" className={hide ? 'hide' : ''}>
-        <div className="content">
-          <p className="info">
-            By accessing and using this website, you acknowledge that you have read and understand our 
-            <a href="cookie-policy">&nbsp;Cookie Policy</a>,
-            <a href="privacy-policy">&nbsp;Privacy Policy</a>, and our
-            <a href="tos">&nbsp;Terms of Service</a>.
-          </p>
-          <button onClick={() => setHide(true)} className="button">
-            Got it
-          </button>
-        </div>
-      </div>
+      <SlideToggle
+        render={({ toggle, setCollapsibleElement }) => (
+          <div id="notification-panel" ref={setCollapsibleElement}>
+            <div className="content">
+              <p className="info">
+                By accessing and using this website, you acknowledge that you have read and understand our 
+                <a href="cookie-policy">&nbsp;Cookie Policy</a>,
+                <a href="privacy-policy">&nbsp;Privacy Policy</a>, and our
+                <a href="tos">&nbsp;Terms of Service</a>.
+              </p>
+              <button onClick={toggle} className="button">
+                Got it
+              </button>
+            </div>
+          </div>
+        )}
+      />
     </>
   )
 }
