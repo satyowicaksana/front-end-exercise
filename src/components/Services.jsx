4b9665638bf1251564d74b78ab3046d1be70c09e
Satyo Wicaksana <satyowicaksana@gmail.com>
2020-01-23 06:58:37 +0700

    add tiles and handle responsiveness in service section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,32 @@
 import React from 'react'
 import './Services.scss'
 
+const tiles = [
+  {
+    title: 'Consult',
+    description: 'Co-create, design thinking; strengthen infrastructure resist granular. Revolution circular, movements or framework social impact low-hanging fruit. Save the world compelling revolutionary progress.'
+  },
+  {
+    title: 'Design',
+    description: 'Policymaker collaborates collective impact humanitarian shared value vocabulary inspire issue outcomes agile. Overcome injustice deep dive agile issue outcomes vibrant boots on the ground sustainable.'
+  },
+  {
+    title: 'Develop',
+    description: 'Revolutionary circular, movements a or impact framework social impact low-hanging. Save the compelling revolutionary inspire progress. Collective impacts and challenges for opportunities of thought provoking.'
+  },
+  {
+    title: 'Marketing',
+    description: 'Co-create, design thinking; strengthen infrastructure resist granular. Revolution circular, movements or framework social impact low-hanging fruit. Save the world compelling revolutionary progress.'
+  },
+  {
+    title: 'Manage',
+    description: 'Change-makers innovation or shared unit of analysis. Overcome injustice outcomes strategize vibrant boots on the ground sustainable. Optimism, effective altruism invest optimism corporate social.'
+  },
+  {
+    title: 'Evolve',
+    description: 'Activate catalyze and impact contextualize humanitarian. Unit of analysis overcome injustice storytelling altruism. Thought leadership mass incarceration. Outcomes big data, fairness, social game-changer.'
+  },
+]
 export default () => {
   return (
     <>
@@ -12,6 +38,18 @@ export default () => {
             Venture philanthropy, revolutionary inclusive policymaker relief. User-centered
             program areas scale.
           </h3>
+          <div className="tiles">
+            {
+              tiles.map(tile => (
+                <div key={tile.title} className="tile-container">
+                  <div className="tile">
+                    <h2>{tile.title}</h2>
+                    <p>{tile.description}</p>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
         </div>
       </div>
     </>
