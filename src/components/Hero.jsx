1c7cedecb2a8b058e0433db925ffb3c3dc4487d0
Satyo Wicaksana <satyowicaksana@gmail.com>
2020-01-22 23:56:17 +0700

    hero content styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,18 @@ export default () => {
     <>
       <div className="hero">
         <div className="container">
-          
+          <img className="logo" src={require('../assets/logo.png')} alt="logo"></img>
+          <div className="content">
+            <h1>Hello! I'm Satyo Wicaksana</h1>
+            <h2>Consult, Design, and Develop Websites</h2>
+            <h3>
+              Have something great in mind? Feel free to contact me.<br />
+              I'll help you to make it happen.
+            </h3>
+          </div>
+          <button>
+            LET'S MAKE CONTACT
+          </button>
         </div>
       </div>
     </>
