62f4b9211f60af25c9affed36f26aace39a5e795
Satyo Wicaksana <satyowicaksana@gmail.com>
2020-01-25 09:09:46 +0700

    Updates

---
+++ precache-manifest.9b08a6a083d3d9af499f7a01e0153e4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29333dfb6a460f68bff0d49c24cde42",
+    "url": "/front-end-exercise/index.html"
+  },
+  {
+    "revision": "a281cf1ec429980aa06e",
+    "url": "/front-end-exercise/static/css/main.17722eec.chunk.css"
+  },
+  {
+    "revision": "e497793eb3cd65964871",
+    "url": "/front-end-exercise/static/js/2.ba09da8a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/front-end-exercise/static/js/2.ba09da8a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a281cf1ec429980aa06e",
+    "url": "/front-end-exercise/static/js/main.6b830575.chunk.js"
+  },
+  {
+    "revision": "df0e13e632da02f2bc62",
+    "url": "/front-end-exercise/static/js/runtime-main.41b0238b.js"
+  },
+  {
+    "revision": "a5a74b4ff9be5b81b4ee10c0f456d8e9",
+    "url": "/front-end-exercise/static/media/hero-background-image.a5a74b4f.jpg"
+  }
+]);
